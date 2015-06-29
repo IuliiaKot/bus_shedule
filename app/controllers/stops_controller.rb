90972195dc5255c4846f3a3481aa1171cc61fa38
IuliiaKot <kotlenko.julia@gmail.com>
2015-06-29 19:34:55 -0400

    show location, load agency, route. stops

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -22,18 +22,18 @@ class StopsController < ApplicationController
   end
 
   def create
-  #   title = params["stop"].split(",")[0]
-  #   lat = params["stop"].split(",")[1].to_f
-  #   lng = params["stop"].split(",")[2].to_f
-  #   @stops = Stop.all
-  #   #stop = Stop.find_by_tag(params)
-  #   @res ={}
-  #   @nearloc = Stop.near([lat, lng], 0.2)
-  #   @nearloc.each do |loc|
-  #     tag = Route.find_by_id(loc["route_id"])
-  #     a  = Stop.get_time_for_stop([tag,loc["tag"].to_i])
-  #
-  #     @res[loc["title"]] = a
-  #   end
+    title = params["stop"].split(",")[0]
+    lat = params["stop"].split(",")[1].to_f
+    lng = params["stop"].split(",")[2].to_f
+    @stops = Stop.all
+    #stop = Stop.find_by_tag(params)
+    @res ={}
+    @nearloc = Stop.near([lat, lng], 0.2)
+    @nearloc.each do |loc|
+      tag = Route.find_by_id(loc["route_id"])
+      a  = Stop.get_time_for_stop([tag,loc["tag"].to_i])
+
+      @res[loc["title"]] = a
+    end
    end
 end
