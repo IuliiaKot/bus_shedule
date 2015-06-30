b2548181d4aaf5d087ffb20c6d7f79a4a7be27be
IuliiaKot <kotlenko.julia@gmail.com>
2015-06-29 21:16:12 -0400

    show location, load agency, route. stops

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -12,16 +12,21 @@ class Stop < ActiveRecord::Base
     response = HTTParty.get("http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=#{route[0].agency.tag}&r=#{route[0].tag}&s=#{route[1]}")
 
     if response.parsed_response["body"]["predictions"]["direction"]
-       response.parsed_response["body"]["predictions"]["direction"]["prediction"].each do |info|
-
-
-         result << info["minutes"]
+       response.parsed_response["body"]["predictions"]["direction"].each do |info, value|
+        if info == "prediction" and value.kind_of?(Array)
+          value.each do |time|
+              result << time["minutes"]
+          end
+        elsif info == "prediction"
+          result << value["minutes"]
+        else
+          result << value
+        end
        end
     else
       return []
     end
-
-    return result << response.parsed_response["body"]["predictions"]["direction"]["title"]
+    return result #<< response.parsed_response["body"]["predictions"]["direction"]["title"]
   end
 
   def self.get_stops(route)
@@ -29,11 +34,8 @@ class Stop < ActiveRecord::Base
     response = HTTParty.get("http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=#{route.agency.tag}&r=#{route.tag}")
 
     response.parsed_response["body"]["route"]["stop"].each do |info|
-    #  if route.stops.find_by(info["tag"]).nil?
-    #    byebug
         route.stops.create(title: info["title"], code: info["stopId"], latitude: info["lat"],
                           longitude: info["lon"], tag: info["tag"])
-    #  end
     end
 
   end
