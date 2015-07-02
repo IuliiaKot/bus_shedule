8cdb649a7a1bfd43c432c394d81db69fb84aa093
IuliiaKot <kotlenko.julia@gmail.com>
2015-07-02 15:34:27 -0400

    find by location

--- app/assets/javascripts/stops.js
+++ app/assets/javascripts/stops.js
@@ -1,28 +1,22 @@
-// $(document).ready(function() {
-// var x = document.getElementById("demo");
-//
-//
-//     if (navigator.geolocation) {
-//         navigator.geolocation.getCurrentPosition(showPosition);
-//     } else {
-//         x.innerHTML = "Geolocation is not supported by this browser.";
-//     }
-//
-//
-// function showPosition(position) {
-//     x.innerHTML = "Latitude: " + position.coords.latitude +
-//     "<br>Longitude: " + position.coords.longitude;
-//
-//     $("form input[name='latitude']").val(position.coords.latitude);
-//     $("form input[name='longitude']").val(position.coords.longitude);
-// };
-// });
 $(document).ready(function() {
+var x = document.getElementById("demo");
 
-navigator.geolocation.getCurrentPosition(setGeoCookie);
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
 
 
-function setGeoCookie(position) {
-var cookie_val = position.coords.latitude + "|" + position.coords.longitude;
-document.cookie = "lat_lng=" + escape(cookie_val);
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude;
+
+    $("form input[name='latitude']").val(position.coords.latitude);
+    $("form input[name='longitude']").val(position.coords.longitude);
+
+    var cookie_val = position.coords.latitude + "|" + position.coords.longitude;
+    document.cookie = "lat_lng=" + escape(cookie_val);
+};
 });
